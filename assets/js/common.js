84ab33a4c947afcef2b8cad8704b30a2298b917d
princessfairyprincess <jessicacmichaels@gmail.com>
2021-04-16 23:57:33 +0200

    add jquery and start functionality. add save button. add and define skintone options. toggler animation.

---
+++ assets/js/common.js
@@ -0,0 +1,16 @@
+$( document ).ready(function() {
+ 
+	//Set up the canvas
+
+	//Option-Colour Toggler
+	const optViewer = $('.options');
+	const optColToggler = $('#togglerInner');
+	const optColState = $('#optColState');
+
+	optColToggler.click(function(){
+		optColState.toggleClass('variants_active colors_active');
+		optViewer.toggleClass('variants_active_view colours_active_view');
+	});
+ 
+});
+
